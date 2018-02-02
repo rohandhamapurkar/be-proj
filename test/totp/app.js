b475e37723a65670b8854f7e0ad31958311510bc
786kaizersoze <dhamapurkar54@gmail.com>
2018-02-02 16:15:44 +0530

    added single page lightweight totp

--- test/totp/app.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.use(require('body-parser').json());
-app.use(require('cors')());
-
-app.use('/',express.static(process.cwd()+'/dist'));
-
-app.listen(5000);
