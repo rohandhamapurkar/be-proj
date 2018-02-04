58d9ac9008ce2b763285a32d4b7d08a337441875
786kaizersoze <dhamapurkar54@gmail.com>
2018-02-04 18:13:46 +0530

    added password rules to signup form

--- front-end/src/components/SignUp.vue
+++ front-end/src/components/SignUp.vue
@@ -2,7 +2,8 @@
     <v-container>
         <v-layout>
             <v-flex xs12 sm6 offset-sm3>
-               <br><h1 class="text-center">User Registration</h1><br>
+                <br>
+                <h1 class="text-center">User Registration</h1><br>
                 <v-card>
                     <v-card-text>
                         <v-container>
@@ -25,7 +26,7 @@
                                 </v-layout>
                                 <v-layout row justify-space-between>
                                     <v-flex xs5>
-                                        <v-text-field name="email" label="Email" id="email" v-model="email" type="email" required></v-text-field>
+                                        <v-text-field name="email" label="Email" id="email" v-model="email" :rules="emailRules" type="email" required></v-text-field>
                                     </v-flex>
                                     <v-flex xs6>
                                         <v-text-field name="mobileNumber" label="Mobile Number" id="mobileNumber" v-model="mobileNumber" type="text" required></v-text-field>
@@ -33,7 +34,7 @@
                                 </v-layout>
                                 <v-layout row>
                                     <v-flex xs12>
-                                        <v-text-field name="password" label="Password" id="password" v-model="password" type="password" required></v-text-field>
+                                        <v-text-field name="password" label="Password" id="password" v-model="password" :rules="passwordRules" type="password" required></v-text-field>
                                     </v-flex>
                                 </v-layout>
                                 <v-layout row>
@@ -44,17 +45,12 @@
                                 <v-layout>
                                     <h3>Upload image for embedded key</h3>
                                 </v-layout>
-                                <v-layout row> 
-                                    <v-btn name="uploadImage">Upload Image</v-btn>    
+                                <v-layout row>
+                                    <v-btn name="uploadImage">Upload Image</v-btn>
                                 </v-layout>
                                 <v-layout row>
                                     <v-flex xs12>
-                                         <v-checkbox
-                                            label="Upload later?"
-                                            v-model="checkbox"
-                                            :rules="[v => !!v || 'You must check to submit!']"
-                                            required
-                                         ></v-checkbox>
+                                        <v-checkbox label="Upload later?" v-model="checkbox" :rules="[v => !!v || 'You must check to submit!']" required></v-checkbox>
                                     </v-flex>
                                 </v-layout>
                                 <v-btn type="submit" @click="submit" color="primary" :disabled="!valid">Sign Up</v-btn>
@@ -70,48 +66,53 @@
 
 
 <script>
-export default {
-    data: () => ({
-        valid: true,
-        firstName: '',
-        lastName: '',
-        username: '',
-        email: '',
-        mobileNumber: '',
-        password: '',
-        confirmPassword: '',
-        uploadImage: '',
-        nameRules: [
-            (v) => !!v || 'Name is required',
-            (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
-        ],
-        checkbox: false
-    }),
-    computed: {
-         comparePasswords() {
-             return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
-         }
-    },
-    methods: {
-        submit () {
-            if (this.$refs.form.validate()) {
-                  // Native form submission is not yet supported
-    //          axios.post('/api/submit', {
-    //            name: this.name,
-    //            email: this.email,
-    //            select: this.select,
-    //            checkbox: this.checkbox
-    //          })
+    export default {
+        data: () => ({
+            valid: true,
+            firstName: '',
+            lastName: '',
+            username: '',
+            email: '',
+            mobileNumber: '',
+            password: '',
+            confirmPassword: '',
+            uploadImage: '',
+            nameRules: [
+                (v) => !!v || 'Name is required',
+                (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
+            ],
+            passwordRules: [
+                (v) => !!v || 'Password is required',
+                (v) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test(v) || 'Password Must be atleast 8 char, 1 lowercase, 1 Uppercase char and 1 number'
+            ],
+
+            checkbox: false
+        }),
+        computed: {
+            comparePasswords() {
+                return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
             }
         },
-        clear () {
-            this.$refs.form.reset()
+        methods: {
+            submit() {
+                if (this.$refs.form.validate()) {
+                    // Native form submission is not yet supported
+                    //          axios.post('/api/submit', {
+                    //            name: this.name,
+                    //            email: this.email,
+                    //            select: this.select,
+                    //            checkbox: this.checkbox
+                    //          })
+                }
+            },
+            clear() {
+                this.$refs.form.reset()
+            }
         }
     }
-}
 </script>
 
 
 <style>
-
+    
 </style>
